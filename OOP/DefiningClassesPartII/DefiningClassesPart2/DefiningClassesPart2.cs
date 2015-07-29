73634c60c5abd8df5bd3bb0c2fdd446a1040bc84
Redsart <redsartvimes@yahoo.com>
2015-07-30 01:09:56 +0300

    Added Calculator app.

---
+++ OOP/DefiningClassesPartII/DefiningClassesPart2/DefiningClassesPart2.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DefiningClassesPart2
+{
+    class DefiningClassesPart2
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
