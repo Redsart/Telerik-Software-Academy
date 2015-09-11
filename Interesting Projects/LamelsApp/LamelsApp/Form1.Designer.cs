d4ad1ac9288ec7930de5cd8ffe6da93a986f1787
Redsart <redsartvimes@yahoo.com>
2015-09-12 00:14:15 +0300

    Added Lamels Application.

---
+++ Interesting Projects/LamelsApp/LamelsApp/Form1.Designer.cs
@@ -0,0 +1,235 @@
+﻿namespace LamelsApp
+{
+    partial class Laminator
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Laminator));
+            this.klient = new System.Windows.Forms.TextBox();
+            this.kvadratiStek = new System.Windows.Forms.TextBox();
+            this.lameliStek = new System.Windows.Forms.TextBox();
+            this.calculate = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // klient
+            // 
+            this.klient.Location = new System.Drawing.Point(141, 22);
+            this.klient.Name = "klient";
+            this.klient.Size = new System.Drawing.Size(100, 20);
+            this.klient.TabIndex = 0;
+            this.klient.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.klient_KeyPress);
+            // 
+            // kvadratiStek
+            // 
+            this.kvadratiStek.Location = new System.Drawing.Point(141, 60);
+            this.kvadratiStek.Name = "kvadratiStek";
+            this.kvadratiStek.Size = new System.Drawing.Size(100, 20);
+            this.kvadratiStek.TabIndex = 1;
+            this.kvadratiStek.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.kvadratiStek_KeyPress);
+            // 
+            // lameliStek
+            // 
+            this.lameliStek.Location = new System.Drawing.Point(141, 98);
+            this.lameliStek.Name = "lameliStek";
+            this.lameliStek.Size = new System.Drawing.Size(100, 20);
+            this.lameliStek.TabIndex = 2;
+            this.lameliStek.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.lameliStek_KeyPress);
+            // 
+            // calculate
+            // 
+            this.calculate.Location = new System.Drawing.Point(141, 140);
+            this.calculate.Name = "calculate";
+            this.calculate.Size = new System.Drawing.Size(75, 23);
+            this.calculate.TabIndex = 3;
+            this.calculate.Text = "Пресметни";
+            this.calculate.UseVisualStyleBackColor = true;
+            this.calculate.Click += new System.EventHandler(this.calculate_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(147, 179);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(22, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = ",,,,,";
+            this.label1.Click += new System.EventHandler(this.label1_Click_1);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(122, 207);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(22, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = ",,,,,";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 29);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(121, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Количество от клиент:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 67);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(93, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Квадрати в стек:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 105);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(85, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Ламели в стек:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(12, 179);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(73, 13);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "За товарене:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(12, 207);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(70, 13);
+            this.label7.TabIndex = 10;
+            this.label7.Text = "По система:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(93, 179);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(51, 13);
+            this.label8.TabIndex = 11;
+            this.label8.Text = "стекове:";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(194, 179);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(48, 13);
+            this.label9.TabIndex = 12;
+            this.label9.Text = "ламели:";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(248, 179);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(22, 13);
+            this.label10.TabIndex = 13;
+            this.label10.Text = ",,,,,";
+            this.label10.Click += new System.EventHandler(this.label10_Click);
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(247, 256);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(44, 13);
+            this.label11.TabIndex = 14;
+            this.label11.Text = "VenArts";
+            // 
+            // Laminator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.YellowGreen;
+            this.ClientSize = new System.Drawing.Size(300, 278);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.calculate);
+            this.Controls.Add(this.lameliStek);
+            this.Controls.Add(this.kvadratiStek);
+            this.Controls.Add(this.klient);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Laminator";
+            this.Text = "Laminator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox klient;
+        private System.Windows.Forms.TextBox kvadratiStek;
+        private System.Windows.Forms.TextBox lameliStek;
+        private System.Windows.Forms.Button calculate;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+    }
+}
+
---
+++ Interesting Projects/LamelsApp/LamelsApp/Form1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LamelsApp
+{
+    public partial class Laminator : Form
+    {
+        public Laminator()
+        {
+            InitializeComponent();
+        }
+
+        private void klient_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            char ch = e.KeyChar;
+
+            if (ch == 46 && klient.Text.IndexOf('.') != -1)
+            {
+                e.Handled = true;
+                return;
+            }
+
+            if (!char.IsDigit(ch) && ch != 8 && ch != 46)
+            {
+                e.Handled = true;
+                
+            }
+        }
+
+        private void kvadratiStek_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            char ch = e.KeyChar;
+
+            if (ch == 46 && kvadratiStek.Text.IndexOf('.') != -1)
+            {
+                e.Handled = true;
+                return;
+            }
+
+            if (!char.IsDigit(ch) && ch != 8 && ch != 46)
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void lameliStek_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            char ch = e.KeyChar;
+
+            if (ch == 46 && lameliStek.Text.IndexOf('.') != -1)
+            {
+                e.Handled = true;
+                return;
+            }
+
+            if (!char.IsDigit(ch) && ch != 8 && ch != 46)
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void calculate_Click(object sender, EventArgs e)
+        {
+            //counting m^2 of 1 lamel
+            double lamelSquare = double.Parse(kvadratiStek.Text) / double.Parse(lameliStek.Text);
+            //numbers of lamels
+            double lamels = double.Parse(klient.Text) / lamelSquare;
+            //counting full stack's
+            int finalStacks = (int)lamels / int.Parse(lameliStek.Text);
+            double current = (finalStacks * int.Parse(lameliStek.Text)) * lamelSquare;
+
+            //stack's are founded lets found the rest
+            double rest = double.Parse(klient.Text) - current;
+            double restLamels = rest / lamelSquare;
+            int finalLamels = (int)restLamels + 1;
+            int test = finalLamels;
+            //for write to the system
+            double sistem = current + finalLamels * lamelSquare;
+            
+            label1.Text = finalStacks.ToString(); //stacks
+            label10.Text = test.ToString(); //lamels
+            label2.Text = sistem.ToString(); //for the system
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label10_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
