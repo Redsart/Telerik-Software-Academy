f808bab1f74fb7da86d3e30dcb714dd395a8d07f
Redsart <redsartvimes@yahoo.com>
2015-10-07 19:11:26 +0300

    Added JavaScript Fundamentals

---
+++ JavaScriptFundamentals/DataTypesAndVariables/JS-Literals.js
@@ -0,0 +1,32 @@
+﻿//Problem 1. JavaScript literals
+//Assign all the possible JavaScript literals to different variables.
+
+//Problem 3. Typeof variables
+//Try typeof on all variables you created.
+
+//Problem 4. Typeof null
+//Create null, undefined variables and try typeof on them.
+
+var array = [1, 2, 3],
+    anotherArray = ['Apple', 'Banana', 'Orange'],
+    integer = 255,
+    float = 255.55,
+    hex = '7b',
+    dec = '1100',
+    boolean = isNaN(9),
+    obj = { firstName: 'Ivan', lastName: 'Ivanov' },
+    str = 'this is string',
+    nullvalue = null,
+    undefinedValue = undefined;
+
+var variables = [array, anotherArray, integer, float, hex, dec, boolean, obj, str, nullvalue, undefinedValue];
+
+for(var variable in variables){
+    console.log(getTypeString(variables[variable]));
+}
+
+function getTypeString(obj) {
+    var result = obj;
+    result += ' is ' + typeof obj;
+    return result;
+}
---
+++ JavaScriptFundamentals/DataTypesAndVariables/QuotedText.js
@@ -0,0 +1,10 @@
+﻿//Problem 2. Quoted text
+//Create a string variable with quoted text in it.
+//For example: `'How you doin'?', Joey said'.
+
+var singleQuotes = '\'How you doin\'?\', Joey said.';
+var doubleQuotes = "'How you doin'?', Joey said.";
+
+var result = singleQuotes + '\n\r' + doubleQuotes;
+
+console.log(result);
