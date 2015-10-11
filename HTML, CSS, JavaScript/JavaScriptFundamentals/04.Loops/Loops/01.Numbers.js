74c2ac2b7ea1652633d762d364b83f6ef3bc2126
Redsart <redsartvimes@yahoo.com>
2015-10-11 21:35:25 +0300

    Added Loops HW

---
+++ HTML, CSS, JavaScript/JavaScriptFundamentals/04.Loops/Loops/01.Numbers.js
@@ -0,0 +1,10 @@
+﻿//Problem 1. Numbers
+//Write a script that prints all the numbers from 1 to N
+
+var number = window.prompt(('Enter number: '));
+var counter = 0;
+
+for (var i = 0; i < number; i++) {
+    console.log(counter);
+    counter += 1;
+}
